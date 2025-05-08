a366b8bb0175605c608efd5821d227aae5b6f567
slpboy <binancesenura@gmail.com>
2025-05-08 17:58:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 require('dotenv').config(); 
 
 global.owner = process.env.OWNER_NUMBER; 
-global.anitav4 = process.env.BOT_NAME || "SENUX MD BOT ✨️🇾🇪💛";
-global.ownername = process.env.OWNER_NAME || "CSWSENU";
-global.packname = process.env.PACK_NAME || "SENURA LAKSHAN 🌏❤️";
-global.author = process.env.AUTHOR || "SLP";
-global.channelname = process.env.CHANNEL_NAME || "කාටුන් ලෝකේ 💛";
-global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
+global.anitav4 = process.env.BOT_NAME || "💗🫟SENU x ANITHA MD BOT 💗🫟";
+global.ownername = process.env.OWNER_NAME || "Mr.Senura Lakshan";
+global.packname = process.env.PACK_NAME || "💚SLP TO CSW";
+global.author = process.env.AUTHOR || "🫟SENURA LAKSHAN🦋";
+global.channelname = process.env.CHANNEL_NAME || "💗කාටූන් ලෝකේ 🔊";
+global.channeljid = process.env.CHANNEL_JID || "120363400978174539@newsletter";
 global.autoTyping = process.env.AUTO_TYPING === 'false'; 
 global.autoRecord = process.env.AUTO_RECORD === 'false';
-global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'true';
+global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'false';
 global.AUTOSTATUS_REACT = process.env.AUTOSTATUS_REACT === 'true';
-global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "🙃";
+global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💗,🌷";
 
 global.LEVELUP = process.env.LEVELUP === 'true';
 
@@ -32,15 +32,15 @@ global.roles = {
 global.public = process.env.PUBLIC === 'true';
 
 global.ANTIDELETE = process.env.ANTIDELETE === 'true';
-global.unavailable = process.env.UNAVAILABLE === 'true';
+global.unavailable = process.env.UNAVAILABLE === 'false';
 global.available = process.env.AVAILABLE === 'true';
 global.autoreadmessages = process.env.AUTO_READ_MESSAGES === 'false';
 global.chatbot = process.env.CHATBOT === 'false';
-global.autoreact = process.env.AUTO_REACT === 'true';
+global.autoreact = process.env.AUTO_REACT === 'false';
 
 global.WELCOME = process.env.WELCOME === 'true';
 
-global.prefix = process.env.PREFIX || ',';
+global.prefix = process.env.PREFIX || '.';
 
 global.autobio = process.env.AUTO_BIO === 'true';
 
@@ -55,12 +55,12 @@ global.mess = {
     admin: process.env.MESSAGE_ADMIN || '_*❗This Command Can Only Be Used By Group Admins !*_',
     botAdmin: process.env.MESSAGE_BOT_ADMIN || '_*❗This Command Can Only Be Used When Bot Becomes Group Admin !*_',
     OnlyOwner: process.env.MESSAGE_OWNER || '_*❗This Command Can Only Be Used By My Owner !*_',
-    OnlyGrup: process.env.MESSAGE_GROUP || '_*❗This Command Can Only Be Used In Group Chat !*_',
-    private: process.env.MESSAGE_PRIVATE || '_*❗This Command Can Only Be Used In Private Chat !*_',
-    wait: process.env.MESSAGE_WAIT || '_*Please Wait*_',
+    OnlyGrup: process.env.MESSAGE_GROUP || '_*❗This Command Can Only Be Used In Group Chat ![ඒක GROUP COMMAND ]*_',
+    private: process.env.MESSAGE_PRIVATE || '_*❗This Command Can Only Be Used In Private Chat ![ඒක PRIVATE]*_',
+    wait: process.env.MESSAGE_WAIT || '_*WAIT MENIKA🌝💙*_',
     notregist: process.env.MESSAGE_NOT_REGIST || '_*You are not registered in the Bot Database. Please register first*_',
-    premium: process.env.MESSAGE_PREMIUM || '_*Premium only" Want Premium? Chat Owner*_',
-    endLimit: process.env.MESSAGE_END_LIMIT || '_*Your Daily Limit Has Been Used Up, The Limit Will Be Reset Every 00:00 AM_*.',
+    premium: process.env.MESSAGE_PREMIUM || '_*ප්‍රිමියම් නෑ සුද්දා🌝🤦🏻‍♂️*_',
+    endLimit: process.env.MESSAGE_END_LIMIT || '10000',
 };
 
 
